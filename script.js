42c51342839e50bacf548ce883011ec75122e31e
Yuri-9 <vashkevichyp@gmail.com>
2019-11-06 00:44:48 +0300

    feat: add handkeyboard input

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 let ArrayKeyboard =[
     ['Backquote', 'ё', 'Ё', '`', '~'],
     ['Digit1', '1', '!', '1', '!'],
@@ -66,9 +67,10 @@ let ArrayKeyboard =[
 
 let arrayKeyCodeChange = [0, 1, 2 , 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 15 , 16, 17 ,18 ,19, 20, 21 , 22, 23, 24, 25, 26, 27, 30, 31, 32 , 33, 34, 35, 36, 37, 38, 39, 40, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52];
 
-
-
-
+let ArrayKeyChange = [];
+for (let i = 0; i < arrayKeyCodeChange.length; i++){
+    ArrayKeyChange[i] = ArrayKeyboard[arrayKeyCodeChange[i]];       
+} 
 
 let sheetDiv = document.createElement('div');
 sheetDiv.className = 'input_sheet';
@@ -106,23 +108,28 @@ for (let i = 1; i <= 5; i++){
         }
     }
  
+    for (let i = 0; i < ArrayKeyChange.length; i++){
+        let a = ArrayKeyChange[i][0];           
+        document.querySelector('#' + a).classList.add('itemChange');
+    }
+
 function changeKeyboard (numberKey) {
-    for (let i = 0; i < arrayKeyCodeChange.length; i++){
-        let a = ArrayKeyboard[arrayKeyCodeChange[i]][0];
-        let b = ArrayKeyboard[arrayKeyCodeChange[i]][numberKey];    
+    for (let i = 0; i < ArrayKeyChange.length; i++){
+        let a = ArrayKeyChange[i][0];
+        let b = ArrayKeyChange[i][numberKey];    
         document.querySelector('#' + a).innerHTML = b;
     }
 }
 
-var SocketValue = localStorage.getItem('numSocket');
-(SocketValue == 'en') ? changeKeyboard(3) : changeKeyboard(1);
+document.querySelector('div.input_sheet').insertAdjacentHTML('beforeend', '|');
+let numKeyboard;
+let SocketValue = localStorage.getItem('numSocket');
+(SocketValue == 'en') ? (changeKeyboard(3), numKeyboard = 3)  : (changeKeyboard(1), numKeyboard = 1)
 console.log (SocketValue);
 
-
-let numKeyboard = 1;
-
-const clickDown = (event) => {
-    if (event.target.id == 'CapsLock') {
+const clickDownMouse = (event) => {
+    
+    if ((event.target.id == 'CapsLock') || (event.code == 'CapsLock')) {
         if ((numKeyboard == 1) || (numKeyboard == 3)) {
             numKeyboard = numKeyboard + 1;
             changeKeyboard(numKeyboard);                    
@@ -131,16 +138,18 @@ const clickDown = (event) => {
             numKeyboard = numKeyboard - 1;
             changeKeyboard(numKeyboard);            
             document.querySelector('#CapsLock').classList.remove('clickBtnCape');     
-        }
-        return;
+        }       
     } 
-    if (event.target.id == 'AltLeft') {
+    document.querySelector('div.input_sheet').lastChild.remove();
+        
+    
+    if ((event.target.id == 'AltLeft') || (event.code == 'AltLeft')){
         document.querySelector('#AltLeft').classList.add('clickBtnCape');
     }  
-    if (event.target.id == 'ControlLeft') {
+    if ((event.target.id == 'ControlLeft') || (event.code == 'ControlLeft')) {
         document.querySelector('#ControlLeft').classList.add('clickBtnCape');
     } 
-    if (event.target.id == 'ShiftLeft') {
+    if ((event.target.id == 'ShiftLeft')  || (event.code == 'ShiftLeft')){
         document.querySelector('#ShiftLeft').classList.add('clickBtnCape');
         if ((document.querySelector('#AltLeft').classList[1] == 'clickBtnCape') || (document.querySelector    ('#ControlLeft').classList[1] == 'clickBtnCape')) {
             if ((numKeyboard == 1) || (numKeyboard == 2)) {
@@ -161,35 +170,71 @@ const clickDown = (event) => {
         }
 
     }  
+    if ((event.target.id == 'Backspace') || (event.code == 'Backspace')) {
+        document.querySelector('#Backspace').classList.add('clickBtnCape');
+        document.querySelector('div.input_sheet').lastChild.remove();
+    }
+    if ((event.target.id == 'Enter') || (event.code == 'Enter')) {
+        document.querySelector('#Enter').classList.add('clickBtnCape');
+        document.querySelector('div.input_sheet').insertAdjacentHTML('beforeend', '<br>');
+    }
+    if ((event.target.id == 'Tab') || (event.code == 'Tab')) {
+        document.querySelector('#Tab').classList.add('clickBtnCape');
+        document.querySelector('div.input_sheet').insertAdjacentHTML('beforeend', '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;');        
+    }
+    if ((event.target.id == 'Space') || (event.code == 'Space')) {
+        document.querySelector('#Space').classList.add('clickBtnCape');
+        document.querySelector('div.input_sheet').insertAdjacentHTML('beforeend', '&nbsp');        
+    }
 
-    if (event.target.className == 'item') {
+
+    if (event.target.classList[1] == 'itemChange') {       
+                      
+        const tr = event.target.innerText;
+        document.querySelector('div.input_sheet').insertAdjacentText('beforeend', tr);
+        
+
         document.querySelector('#AltLeft').classList.remove('clickBtnCape');
         document.querySelector('#ControlLeft').classList.remove('clickBtnCape');
-        document.querySelector('#ShiftLeft').classList.remove('clickBtnCape');
-         
-        event.target.classList.add('clickBtn'); 
-        if (event.target.id == 'Backspace') {
-            document.querySelector('div.input_sheet').lastChild.remove();
-        } else if (event.target.id == 'Enter') {
-            document.querySelector('div.input_sheet').insertAdjacentHTML('beforeend', '<br>');
-        } else if (event.target.id == 'Tab') {
-            document.querySelector('div.input_sheet').insertAdjacentHTML('beforeend', '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;');
-        } else {            
-            const tr = event.target.innerText;
-            document.querySelector('div.input_sheet').insertAdjacentText('beforeend', tr);
-        } 
+        if (document.querySelector('#ShiftLeft').classList[1] == 'clickBtnCape'){
+            if ((numKeyboard == 2) || (numKeyboard == 4)) {
+                numKeyboard = numKeyboard - 1;
+                changeKeyboard(numKeyboard);
+                
+                document.querySelector('#ShiftLeft').classList.remove('clickBtnCape');
+            }
+        }
+    }   
+
+    for (let i = 0; i < ArrayKeyChange.length; i++) {
+        let idKey = ArrayKeyChange[i][0];
+        if (event.code === idKey) {
+            document.querySelector('div.input_sheet').insertAdjacentText('beforeend', ArrayKeyChange[i][numKeyboard]);
+            document.querySelector('#' + idKey).classList.add('clickBtn');        
+        }   
     }
-}   
-const clickUp = () => {
-    document.querySelectorAll('.item').forEach(item => item.classList.remove('clickBtn'));
+  
+   document.querySelector('div.input_sheet').insertAdjacentHTML('beforeend', '|');
+
+}
+const clickUpMouse = () => {
+    document.querySelector('#Backspace').classList.remove('clickBtnCape');
+    document.querySelector('#Enter').classList.remove('clickBtnCape');
+    document.querySelector('#Tab').classList.remove('clickBtnCape');
+    document.querySelector('#Space').classList.remove('clickBtnCape');
+
+    document.querySelectorAll('.itemChange').forEach(item => item.classList.remove('clickBtn'));
     if  (((document.querySelector('#AltLeft').classList[1] == 'clickBtnCape') || (document.querySelector('#ControlLeft').classList[1] == 'clickBtnCape')) && (document.querySelector('#ShiftLeft').classList[1] == 'clickBtnCape')) {
         document.querySelector('#AltLeft').classList.remove('clickBtnCape');
         document.querySelector('#ControlLeft').classList.remove('clickBtnCape');
         document.querySelector('#ShiftLeft').classList.remove('clickBtnCape');
     }
 }  
-document.querySelector('.keyboarder_wrapper').addEventListener('mousedown', clickDown);
-document.addEventListener('mouseup', clickUp);
+document.querySelector('.keyboarder_wrapper').addEventListener('mousedown', clickDownMouse);
+document.addEventListener('mouseup', clickUpMouse);
+
+document.addEventListener('keydown',clickDownMouse);
+document.addEventListener('keyup',clickUpMouse);
 
 
 
